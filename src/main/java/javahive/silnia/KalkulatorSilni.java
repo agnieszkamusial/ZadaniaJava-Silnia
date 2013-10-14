a4bafecd6a98abea6f243c5618ea22d3b0dfaf60
student <student@212.182.25.148>
2013-10-14 18:54:04 +0200

    kom

---
+++ src/main/java/javahive/silnia/KalkulatorSilni.java
@@ -0,0 +1,12 @@
+
+package javahive.silnia;
+/**
+ * uwaga - test gita
+ * 
+ * test
+ * @author Marcin Grabowiecki
+ */
+
+public interface KalkulatorSilni {
+    public String licz(int arg) throws ArithmeticException;
+}
